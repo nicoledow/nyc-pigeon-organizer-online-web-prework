ad855d57f3b5c308e49eafed7a836fd35b454bba
Nicole Dow <nikki.dow71@gmail.com>
2019-05-23 21:03:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -75,7 +75,7 @@ def nyc_pigeon_organizer(data)
     end
   end
  
- #adding a "color" key to each bird whose value is an array containing all its colors 
+ #adding a "color" key to each bird in pigeon_list, whose value is an array containing all its colors 
  data[:color].each do |color, group_of_birds|
    group_of_birds.each do |bird|
     if pigeon_list[bird].has_key?(:color)
@@ -87,13 +87,22 @@ def nyc_pigeon_organizer(data)
    end
  end
  
- #adding a "gender" key to each bird whose value is an array
+ #adding a "gender" key to each bird in pigeon_list, whose value is an array
  data[:gender].each do |gender, group_of_birds|
    group_of_birds.each do |bird|
       pigeon_list[bird][:gender] = []
       pigeon_list[bird][:gender] << gender.to_s
     end
  end
+  
+  #adding a "lives" key to each bird in pigeon_list, whoes value is their location
+  data[:lives].each do |location, group_of_birds|
+    group_of_birds.each do |bird|
+      pigeon_list[bird][:lives] = []
+      pigeon_list[bird][:lives] << location.to_s
+    end
+  end
+  
   
   pigeon_list
 end
